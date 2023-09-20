767c7dbf1df12dab3ab95a15885ae74c7bbee551
primusr <primus.reyes@gmail.com>
2023-09-20 14:01:36 +0800

    first php script

---
+++ demo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First PHP Script</title>
+</head>
+<body>
+    <?php 
+        echo "PHP Demo";
+        echo "<br> blah blah blah";
+        echo "\n New PHP Demo";
+        
+    ?>
+    
+</body>
+</html>
